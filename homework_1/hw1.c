465cc16ae66c638ad8fe55e1b02a05123935f158
WANKIM MOK <cafe.mui@gmail.com>
2015-09-22 00:40:19 -0400

    change background to white, different color for left and right click,Draw() function, and include input text file

--- homework_1/hw1.c
+++ homework_1/hw1.c
@@ -1,5 +1,6 @@
 /* 
 	Compiles with command line  gcc -o test hw1.c -lX11 -lm -L/usr/X11R6/lib 
+	Run : ./test input.txt
 	Homework #1
 	Wan Kim Mok
 	Due: September 30, 2015
@@ -14,31 +15,96 @@
 
 Display *display;
 Window win;
-GC green_gc;
+GC green_gc, red_gc;
 XEvent report;
-XColor green_col;
+XColor green_col, red_col;
 Colormap colormap;
 
 char green[] = "#00FF00";
+char red[] = "#A80000";
+
+
+void Draw(char *argv[]){
+	FILE *fp;
+	char buff[1000];
+	int ch, i;
+	int line_count = 0;
+	
+	fp = fopen(argv[1], "r");
+
+	if (fp == NULL){
+		printf("There's no input file.\n");
+		exit(0);
+	}
+	else{
+		//printf("%s\n", argv[1]);
+		while(1){
+			ch = fgetc(fp);
+			if(feof(fp)){
+				break;
+			}
+			if (ch == '\n'){
+				line_count++;
+			}
+			//printf("%c\n", ch);
+		}
+
+		rewind(fp);
+
+		printf("Total lines:  %d\n", line_count+1);
+
+		/* go through characters per line */
+		int m[line_count][2];
+
+		//XDrawLine(display, win, green_gc, 2, 2, 4,2);
+		
+		for(i=0;i<=line_count;i++){
+			//store formatted input file in array m
+			fscanf(fp, "%d,%d", &m[i][0], &m[i][1]);
+			//printf("%d %d\n", m[i][0], m[i][1]);
+
+			//Magnify points by 2
+			//m[i][0] = (m[i][0]*2);
+			//m[i][1] = (m[i][1]*2);
+
+			printf("Scaled coordinates: %d, %d\n", m[i][0], m[i][1]);
+		}
+
+		for(i=0;i<line_count;i++){
+			/* draw lines */
+			XDrawLine(display, win, green_gc, m[i][0], m[i][1], m[i+1][0], m[i+1][1]);
+		}
+		//XDrawLine(display, win, green_gc, m[i][0], m[i][1], m[0][0], m[0][1]);
+		rewind(fp);
+		
+		printf("Close file.");
+		fclose(fp);
+	}
+}
 
 int main(int argc, char *argv[]){
 	display = XOpenDisplay(NULL);
 
 	//Creating window
-	win = XCreateSimpleWindow(display, RootWindow(display, 0), 1, 1, 500, 500, 10, BlackPixel (display, 0), BlackPixel (display, 0));
+	win = XCreateSimpleWindow(display, RootWindow(display, 0), 1, 1, 500, 500, 10, WhitePixel (display, 0), WhitePixel (display, 0));
 	
 	//Maps window on screen
 	XMapWindow(display, win);
 
 	colormap = DefaultColormap(display, 0);
 	green_gc = XCreateGC(display, win, 0, 0);
+	red_gc = XCreateGC(display, win, 0, 0);
 	XParseColor(display, colormap, green, &green_col);
+	XParseColor(display, colormap, red, &red_col);
 	XAllocColor(display, colormap, &green_col);
+	XAllocColor(display, colormap, &red_col);
 	XSetForeground(display, green_gc, green_col.pixel);
+	XSetForeground(display, red_gc, red_col.pixel);
 
 	XSelectInput(display, win, ExposureMask | KeyPressMask | ButtonPressMask);
 
 	//XDrawRectangle(display, win, green_gc, 10, 10, 398, 398);
+	//Draw(argv);
 
 
 	XFlush(display);
@@ -48,8 +114,10 @@ int main(int argc, char *argv[]){
 		switch(report.type){
 			case Expose:
 				//printf("Exposed.");
-				XDrawRectangle(display, win, green_gc, 50, 50, 398, 398);
-				XDrawLine(display, win, green_gc, 10, 10, 400, 400);
+				//XDrawRectangle(display, win, green_gc, 50, 50, 398, 398);
+				//XDrawLine(display, win, green_gc, 10, 10, 400, 400);
+				//XDrawLine(display, win, green_gc, 1, 1, 400, 400);
+				Draw(argv);
 				XFlush(display);
 				break;
 			case KeyPress:
@@ -60,7 +128,13 @@ int main(int argc, char *argv[]){
 				break;
 			case ButtonPress:
 				printf("Button press %d, %d.\n",report.xbutton.x, report.xbutton.y);
-				XDrawPoint(display, win, green_gc, report.xbutton.x, report.xbutton.y);
+				if (report.xbutton.button == Button1){
+					/* left click */
+					XDrawPoint(display, win, green_gc, report.xbutton.x, report.xbutton.y);
+				}
+				else{
+					XDrawPoint(display, win, red_gc, report.xbutton.x, report.xbutton.y);
+				}
 				break;
 		}
 	}
