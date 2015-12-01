bc9adf1516ca3e16a1a1436146f807b946f824bc
WANKIM MOK <cafe.mui@gmail.com>
2015-11-30 22:41:34 -0500

    remove is_inTriangle function, included some comments to describe some of the functions

--- homework_1/hw1.c
+++ homework_1/hw1.c
@@ -48,26 +48,23 @@ char black[] = "#000000";
 char light_purple[] = "#FFCCFF";
 
 
-int orientation(int ax, int ay, int bx, int by, int cx, int cy)
-{
+int orientation(int ax, int ay, int bx, int by, int cx, int cy){
 	return (ax*by) + (bx*cy) + (cx*ay) - (ay*bx) - (by*cx) - (cy*ax);
 }
 
-int is_inTrangle(int px, int py, int ax, int ay, int bx, int by, int cx, int cy)
-{
-	int r1, r2, r3, r4, r5, r6;
-	r1 = orientation(px, py, ax, ay, bx, by)*orientation(cx, cy, ax, ay, bx, by);
-	r2 = orientation(px, py, bx, by, cx, cy)*orientation(ax, ay, bx, by, cx, cy);
-	r3 = orientation(px, py, ax, ay, cx, cy)*orientation(bx, by, ax, ay, cx, cy);
-	return  r1, r2, r3;
-}
-
-double find_distance(int x1, int y1, int x2, int y2)
-{
+double find_distance(int x1, int y1, int x2, int y2){
+	/*
+		Returns the distance between two points.
+	*/
 	return sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
 }
 
 int check_if_in_triangle(int line_count, int m[][6], int x, int y){
+	/*
+		Checks if a point(vertex) is within another triangle.
+		Return 1 if point is n triangle, else return 0.
+	*/
+
 	int i;
 	for (i = 0; i <= line_count; i++){
 
@@ -87,6 +84,10 @@ int check_if_in_triangle(int line_count, int m[][6], int x, int y){
 }
 
 int check_intersect(int line_count, int vertex[][6], int start_x, int start_y, int target_x, int target_y){
+	/*
+		Checks if the given line intersects with any of the triangle's line.
+		Returns 1 if interesects, else return 0.
+	*/
 
 	int i, j;
 	int val = 0;
@@ -122,7 +123,6 @@ int check_intersect(int line_count, int vertex[][6], int start_x, int start_y, i
 }
 
 int expand(int line_count, int vertex[][6], int m, int n, int current_x, int current_y){
-	
 	int i, j, l;
 	
 	for (i=0; i<=line_count; i++){
@@ -166,18 +166,17 @@ int expand(int line_count, int vertex[][6], int m, int n, int current_x, int cur
 }
 
 void valid_vertices(int line_count, int vertex[][6], int i, int current_x, int current_y){
-	//get all the valid vertices in a new list
+	/*
+		Get all the valid vertices in a valid_vertex[] list.
+	*/
 	
-	printf("========================= VALID_VERTICES() =========================\n");
-
-	int m, n;
+	int m, n, f;
 
 	for (m=0; m<=line_count; m++){
 
 		for(n=0; n<6; n+=2){
 
-			int f = m;
-			printf("F: %d\n", f);
+			f = m;
 
 			while(vertex[f][n]!= -5 && f<=line_count+1){
 
@@ -192,6 +191,10 @@ void valid_vertices(int line_count, int vertex[][6], int i, int current_x, int c
 }
 
 int vertex_exist(int x, int y, int current_x, int current_y){
+	/*
+		Checks if vertex exist.
+		Returns 1 if exist, else 0.
+	*/
 
 	int i;
 	if (current_x == x && current_y == y){
@@ -213,10 +216,48 @@ int vertex_exist(int x, int y, int current_x, int current_y){
 	return 0;
 }
 
-void end_graph(int line_count, int vertex[][6], int target_x, int target_y){
-	
+void start_graph(int line_count, int vertex[][6], int start_x, int start_y, int target_x, int target_y){
+	/*
+		Retrieve all valid vertex that will graph all possible routes in the graph.
+	*/
+
 	int i, j;
 
+	//store the start point in valid_vertex 
+	valid_vertex[k][0] = start_x;	// from x value
+	valid_vertex[k][1] = start_y;   // from y value
+	valid_vertex[k][2] = start_x;	// to x value
+	valid_vertex[k][3] = start_y;	// to y value
+	valid_vertex[k][4] = 0.0;		// distance from point a to b
+	valid_vertex[k][5] = 0.0;		// total distance
+	valid_vertex[k++][6] = 0.0;		// -2.0 target , 1.0 has visited not included, 0.0 using, -1.0 not yet visited
+
+
+	//handles the lines connecting from the starting point and branch out
+	for (i=0; i<=line_count; i++){
+
+		for (j=0; j<6; j+=2){
+
+			if (check_if_in_triangle(line_count, vertex, vertex[i][j], vertex[i][j+1]) == 0){
+
+				if (check_intersect(line_count, vertex, start_x, start_y, vertex[i][j], vertex[i][j+1]) == 0){
+					
+					valid_vertex[k][0] = start_x;
+					valid_vertex[k][1] = start_y;
+					valid_vertex[k][2] = vertex[i][j];
+					valid_vertex[k][3] = vertex[i][j+1];
+					valid_vertex[k][4] = find_distance(vertex[i][j], vertex[i][j+1], start_x, start_y);
+					valid_vertex[k][5] = -1.0;
+					valid_vertex[k++][6] = -1.0;
+					
+					valid_vertices(line_count, vertex, i, vertex[i][j], vertex[i][j+1]);
+
+				}
+			}
+		}
+	}
+
+	//handles the lines connecting to the target point.
 	for(i=0; i<=line_count; i++){
 		for(j=0; j<6; j+=2){
 			if(check_if_in_triangle(line_count, vertex, vertex[i][j], vertex[i][j+1])==0){
@@ -239,54 +280,15 @@ void end_graph(int line_count, int vertex[][6], int target_x, int target_y){
 	}
 }
 
-void start_graph(int line_count, int vertex[][6], int start_x, int start_y, int target_x, int target_y){
-
-	int i, j;
-
-	//store the start point in valid_vertex 
-	valid_vertex[k][0] = start_x;	// from x value
-	valid_vertex[k][1] = start_y;   // from y value
-	valid_vertex[k][2] = start_x;	// to x value
-	valid_vertex[k][3] = start_y;	// to y value
-	valid_vertex[k][4] = 0.0;		// distance from point a to b
-	valid_vertex[k][5] = -1.0;		// total distance
-	valid_vertex[k++][6] = 0.0;		// -2.0 target , 1.0 has visited not included, 0.0 using, -1.0 not yet visited
-
-
-	for (i=0; i<=line_count; i++){
-
-		for (j=0; j<6; j+=2){
-
-			if (check_if_in_triangle(line_count, vertex, vertex[i][j], vertex[i][j+1]) == 0){
-
-				if (check_intersect(line_count, vertex, start_x, start_y, vertex[i][j], vertex[i][j+1]) == 0){
-					
-					printf("to (%d, %d)\n", vertex[i][j], vertex[i][j+1]);
-					
-					valid_vertex[k][0] = start_x;
-					valid_vertex[k][1] = start_y;
-					valid_vertex[k][2] = vertex[i][j];
-					valid_vertex[k][3] = vertex[i][j+1];
-					valid_vertex[k][4] = find_distance(vertex[i][j], vertex[i][j+1], start_x, start_y);
-					valid_vertex[k][5] = -1.0;
-					valid_vertex[k++][6] = -1.0;
-					
-					valid_vertices(line_count, vertex, i, vertex[i][j], vertex[i][j+1]);
-
-				}
-			}
-		}
-	}
-}
-
 
 int smallest_length(int total, int length_list[]){
-
-	printf("========================= SMALLEST_LENGTH() =========================\n");
+	/*
+		Returns the shortest route from the neighboring vertices.
+		If the target point has reached return -100 else the vertex with the shortest path.
+	*/
 	
 	double smallest;
 	int i, j, m, goal;
-	printf("j23: %d\n", length_list[0]);
 
 	if (valid_vertex[length_list[0]][6] == -2.0){
 	}
@@ -319,7 +321,7 @@ int smallest_length(int total, int length_list[]){
 		}
 	}
 
-	printf("smallest: %f %d %d last %f\n", smallest, m, goal, valid_vertex[m][6]);
+	//printf("smallest: %f %d %d last %f\n", smallest, m, goal, valid_vertex[m][6]);
 	if (goal == 1){
 		//destination
 		return -100;
@@ -330,7 +332,6 @@ int smallest_length(int total, int length_list[]){
 }
 
 void check_reverse_seg(int index){
-	printf("========================= CHECK_REVERSE_SEG() =========================\n");
 	int i, p;
 
 	for(i=0; i<k; i++){
@@ -354,8 +355,10 @@ void check_reverse_seg(int index){
 }
 
 int check_alt_path_to_point(int index, int index_of_smallest){
-
-	printf("========================= CHECK_ALT_PATH_TO_POINT() =========================\n");
+	/*
+		Look for alternative path to the current point(vertex).
+		Returns the alternative point with the shortest path.
+	*/
 
 	int i, temp_k, j, m;
 	int temp[6];
@@ -402,12 +405,9 @@ int check_alt_path_to_point(int index, int index_of_smallest){
 }
 
 int dijkstra(int index_of_smallest){
-
-	printf("========================= DIJKSTRA() =========================\n");
 	int i, j,m, goal;
 	int temp[6];
 	j=0;
-
 
 	for (i=0; i<k; i++){
 
@@ -426,7 +426,7 @@ int dijkstra(int index_of_smallest){
 	if (j){
 		//there is a list of neighbors
 		m = smallest_length(j, temp);
-		printf("There are neighbors: %d\n", m);
+		//printf("There are neighbors: %d\n", m);
 		if (m != -100){
 			check_reverse_seg(m);
 		}
@@ -434,7 +434,7 @@ int dijkstra(int index_of_smallest){
 	}
 	else{
 		// no neighbors
-		printf("There is no neighbor: @ %d, %f\n", m, valid_vertex[m][6]);
+		//printf("There is no neighbor: @ %d, %f\n", m, valid_vertex[m][6]);
 		m = check_alt_path_to_point(m, index_of_smallest);
 		//return -100;
 	}
@@ -442,8 +442,11 @@ int dijkstra(int index_of_smallest){
 
 
 void shortest_path(){
+	/*
+		Run Dijkstra's algorithm on the graph to obtain the shortest path.
+	*/
 
-	int i, j, index_of_smallest, next_index, h1, h2, goal;
+	int i, j, index_of_smallest, next_index, goal;
 	int temp[6];// holds which vertex
 	double smallest;
 	j = 0;
@@ -464,17 +467,41 @@ void shortest_path(){
 	}
 
 	index_of_smallest = smallest_length(j, temp);
-	printf("BEGIN: SMALLEST %d\n", index_of_smallest);
 
-	int b;
-	b = index_of_smallest;
+	goal = index_of_smallest;
 
 	while(1){
-		b = dijkstra(b);
-		if (b == -100){
+		goal = dijkstra(goal);
+		if (goal == -100){
 			break;
 		}
 		
+	}
+}
+
+void organize(){
+	/*
+		Sorts valid_vertex[].
+	*/
+
+	double temp[7];
+	int i, j, p;
+
+	for(j=0; j<k-1; j++){
+		for (i=0; i<k-1; i++){
+			if (valid_vertex[i+1][5] < valid_vertex[i][5]){
+				
+				for (p=0; p<7; p++){
+					temp[p] = valid_vertex[i][p];
+				}
+				for (p=0; p<7; p++){
+					valid_vertex[i][p] = valid_vertex[i+1][p];
+				}
+				for (p=0; p<7; p++){
+					valid_vertex[i+1][p] = temp[p];
+				}
+			}
+		}
 	}
 }
 
@@ -489,7 +516,7 @@ int main(int argc, char *argv[]){
 
 	FILE *fp;
 	char buff[1000];
-	int ch, i;
+	int ch, i, j, x, y;
 	int line_count = 0;
 	
 	fp = fopen(argv[1], "r");
@@ -553,12 +580,6 @@ int main(int argc, char *argv[]){
 
 				m[line_count+1][0] = -5;
 
-				printf("line_count: %d\n", line_count);
-
-				
-				int j;
-				
-				
 				for (i = 0; i <= line_count ; i++){
 					for (j=0; j<6; j+=2){
 						m[i][j] = m[i][j] + 100;
@@ -574,13 +595,13 @@ int main(int argc, char *argv[]){
 				}
 
 				rewind(fp);
-				printf("exposed\n");
+				//printf("exposed\n");
 				XFlush(display);
 				break;
 			}
 			case ButtonPress:
-				printf("Button press %d, %d.\n",report.xbutton.x, report.xbutton.y);
-				int x, y;
+			{
+				//printf("Button press %d, %d.\n",report.xbutton.x, report.xbutton.y);
 				double distance1;
 				x = report.xbutton.x;
 				y = report.xbutton.y;
@@ -607,8 +628,7 @@ int main(int argc, char *argv[]){
 					exit(1);
 				}
 
-
-				printf("count: %d\n", count);
+				//printf("count: %d\n", count);
 
 				if (count == 0){
 					start_x = x;
@@ -624,8 +644,9 @@ int main(int argc, char *argv[]){
 						target_x = x;
 						target_y = y;
 
-						printf("Count less than 2\n");
-						printf("point x: %d, point y: %d\n", start_x, start_y);
+						//printf("Count less than 2\n");
+						//printf("point x: %d, point y: %d\n", start_x, start_y);
+						printf("\n\nStarting point: (%d, %d)\nTarget point: (%d, %d)\n", start_x, start_y, target_x, target_y);
 
 						int vertex[line_count][6];
 						int * nearest_triangles;
@@ -637,9 +658,8 @@ int main(int argc, char *argv[]){
 						}
 						rewind(fp);
 						
-						int j;
-						
-						
+						printf("Total triangles: %d\n", line_count);
+
 						for (i = 0; i <= line_count ; i++){
 							for (j=0; j<6; j+=2){
 								vertex[i][j] = vertex[i][j] + 100;
@@ -649,7 +669,7 @@ int main(int argc, char *argv[]){
 
 						start_graph(line_count, vertex, start_x, start_y, target_x, target_y);
 
-						end_graph(line_count, vertex, target_x, target_y);
+						printf("Applied start_graph()\n");
 						
 						/*
 						for (i= 0; i<k; i++){
@@ -658,50 +678,43 @@ int main(int argc, char *argv[]){
 						}
 						*/
 						
-						
+						/*
 						printf(" total lines %d\n", k);
 						
-						/*
 						for (i= 0; i<k; i++){
 							if  (valid_vertex[i][0] != 0){
 								XDrawLine(display, win, light_purple_gc, valid_vertex[i][0], valid_vertex[i][1], valid_vertex[i][2], valid_vertex[i][3]);
 							}
 						}
+
+						for(i=0; i<k; i++){
+							printf("from (%d, %d) to (%d, %d) is %f long and weight: %f and has visited value: %d.\n", (int)valid_vertex[i][0], (int)valid_vertex[i][1], (int)valid_vertex[i][2], (int)valid_vertex[i][3], valid_vertex[i][4], valid_vertex[i][5], (int)valid_vertex[i][6]);
+						}
 						*/
 
-						
-						for(i=0; i<k; i++){
-							//printf("from (%d, %d) to (%d, %d) is %f long and %f.\n", (int)valid_vertex[i][0], (int)valid_vertex[i][1], (int)valid_vertex[i][2], (int)valid_vertex[i][3], valid_vertex[i][4], valid_vertex[i][5]);
-							printf("from (%d, %d) to (%d, %d) is %f long and weight: %f and has visited value: %d.\n", (int)valid_vertex[i][0], (int)valid_vertex[i][1], (int)valid_vertex[i][2], (int)valid_vertex[i][3], valid_vertex[i][4], valid_vertex[i][5], (int)valid_vertex[i][6]);
-						}
-
-						
 						shortest_path();
 
-						printf("apply shortest_path()\n");
+						printf("Applied shortest_path()\n");
 
-						
+						/*
 						for(i=0; i<k; i++){
 							printf("from (%d, %d) to (%d, %d) is %f long and weight: %f and has visited value: %d.\n", (int)valid_vertex[i][0], (int)valid_vertex[i][1], (int)valid_vertex[i][2], (int)valid_vertex[i][3], valid_vertex[i][4], valid_vertex[i][5], (int)valid_vertex[i][6]);
 						}
-						
-						
+						*/
+
+						organize();
+
 						printf("The shortest path: \n");
+
 						for (i= 0; i<k; i++){
 							if  (valid_vertex[i][6] == 0.0 || valid_vertex[i][6] == -4.0){
-								printf("from (%d, %d) to (%d, %d) is %f long and last %f.\n", (int)valid_vertex[i][0], (int)valid_vertex[i][1], (int)valid_vertex[i][2], (int)valid_vertex[i][3], valid_vertex[i][4], valid_vertex[i][6]);		
+								printf("from (%d, %d) to (%d, %d) length: %f and path length: %f\n", (int)valid_vertex[i][0], (int)valid_vertex[i][1], (int)valid_vertex[i][2], (int)valid_vertex[i][3], valid_vertex[i][4], valid_vertex[i][5]);
 								XDrawLine(display, win, light_purple_gc, valid_vertex[i][0], valid_vertex[i][1], valid_vertex[i][2], valid_vertex[i][3]);
 							}
 						}
 						
-						
-						printf(" total lines2 %d\n", k);
-						
-						
 						printf("DONE\n");
 						
-
-
 						count = -1;
 					}
 					else{
@@ -709,13 +722,9 @@ int main(int argc, char *argv[]){
 					}
 				}
 
-				//if (inTriangle != 0){
-				//	count = -1;
-				//}
-				
-				//Draw(argv);
 				XFlush(display);
 				break;
+			}
 			default:
 				break;
 		}
