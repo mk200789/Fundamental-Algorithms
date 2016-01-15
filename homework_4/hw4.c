998dbca46b0c16f23e6ec8871707b4e8070e95ca
WANKIM MOK <cafe.mui@gmail.com>
2016-01-14 19:52:48 -0500

    graphics setup

---
+++ homework_4/hw4.c
@@ -0,0 +1,169 @@
+#include <X11/Xlib.h>
+#include <X11/Xutil.h>
+#include <X11/Xos.h>
+#include <X11/Xatom.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+Display *display;
+Screen *screen;
+unsigned int display_width, display_height;
+Window win;
+unsigned int win_width, win_height;
+char *display_name = NULL;
+int screen_name;
+int screen_num;
+int border_width;
+int win_x, win_y;
+
+XWMHints *wm_hints;
+XClassHint *class_hints;
+XSizeHints *size_hints;
+XTextProperty win_name, icon_name;
+char *win_name_string = "Homework 4";
+char *icon_name_string = "Icon for homework 4 window";
+
+XEvent report;
+
+GC gc, green_gc, red_gc, black_gc, blue_gc;
+XColor green_col, red_col, black_col, blue_col;
+Colormap colormap;
+
+char green[] = "#1F8728";
+char red[] = "#FF3333";
+char black[] = "#000000";
+char blue[] = "#0000FF";
+
+int main(int argc, char *argv[]){
+	/*
+	FILE *fp;
+	fp = fopen(argv[1], "r");
+
+	int previous_val = 0;
+	if (fp == NULL){
+		//If there's no file, print message and exit.
+		printf("There's no file.\n");
+		exit(0);
+	}
+	else{
+		if( (display = XOpenDisplay(display_name)) == NULL ){ 
+			printf("Could not open display. \n"); 
+			exit(-1);
+		}
+		printf("Connected to X server  %s\n", XDisplayName(display_name) );
+		
+		screen_num = DefaultScreen(display);
+		screen = DefaultScreenOfDisplay(display);
+		colormap = XDefaultColormap(display, screen_num);
+		display_width = DisplayWidth(display, screen_num);
+		display_height = DisplayHeight(display, screen_num);
+
+	}
+*/
+
+	if( (display = XOpenDisplay(display_name)) == NULL ){ 
+		printf("Could not open display. \n"); 
+		exit(-1);
+	}
+	printf("Connected to X server  %s\n", XDisplayName(display_name) );
+	
+	screen_num = DefaultScreen(display);
+	screen = DefaultScreenOfDisplay(display);
+	colormap = XDefaultColormap(display, screen_num);
+	display_width = DisplayWidth(display, screen_num);
+	display_height = DisplayHeight(display, screen_num);
+
+	//Creating window
+	border_width = 10;
+	win_x = 0; win_y = 0;
+	win_width = display_width/2;
+	win_height = display_height * 0.8;
+	//win_height = (int)(win_width/1.7); //rectangular window
+
+	printf("window width: %d\n window height: %d\n", display_width, display_height);
+
+	//win = XCreateSimpleWindow(display, RootWindow(display, 0), 1, 1, win_width, win_height, 10, WhitePixel (display, 0), WhitePixel (display, 0));
+	win = XCreateSimpleWindow(display, RootWindow(display, screen_num), 
+		win_x, win_y, win_width, win_height, border_width,
+		BlackPixel(display, screen_num), WhitePixel(display, screen_num));
+	
+
+	//Maps window on screen
+	size_hints = XAllocSizeHints();
+	wm_hints = XAllocWMHints();
+	class_hints = XAllocClassHint();
+	if (size_hints == NULL || wm_hints == NULL || class_hints == NULL){
+		printf("Error allocating memory for hints\n");
+		exit(-1);
+	}
+
+	size_hints -> flags = PPosition | PSize | PMinSize;
+	size_hints -> min_width = 60;
+	size_hints -> min_height = 60;
+
+	XStringListToTextProperty(&win_name_string, 1, &win_name);
+	XStringListToTextProperty(&icon_name_string, 1, &icon_name);
+
+	wm_hints -> flags = StateHint | InputHint;
+	wm_hints -> initial_state = NormalState;
+	wm_hints -> input = False;
+
+	class_hints -> res_name = "x_use_example";
+	class_hints -> res_class = "homework3";
+	
+	XSetWMProperties(display, win, &win_name, &icon_name, argv, argc, size_hints, wm_hints, class_hints );
+	XSelectInput(display, win, ExposureMask | KeyPressMask | ButtonPressMask);
+
+	// put on screen
+	XMapWindow(display, win);
+	XFlush(display);
+
+	//graphics setup
+	green_gc = XCreateGC(display, win, 0, 0);
+	XParseColor(display, colormap, green, &green_col);
+	XSetLineAttributes(display, green_gc, 4, LineSolid, CapRound, JoinRound);
+	if (XAllocColor(display, colormap, &green_col) == 0){
+		printf("Failed to get color green\n");
+		exit(-1);
+	}
+	else{
+		XSetForeground(display, green_gc, green_col.pixel);
+
+	}
+
+	red_gc = XCreateGC(display, win, 0, 0);
+	XParseColor(display, colormap, red, &red_col);
+	XSetLineAttributes(display, red_gc, 2, LineSolid, CapRound, JoinRound);
+	if (XAllocColor(display, colormap, &red_col) == 0){
+		printf("Failed to get color red\n");
+		exit(-1);
+	}
+	else{
+		XSetForeground(display, red_gc, red_col.pixel);
+	}
+
+
+	black_gc = XCreateGC(display, win, 0, 0);
+	XParseColor(display, colormap, black, &black_col);
+	if (XAllocColor(display, colormap, &black_col) == 0){
+		printf("Failed to get color black\n");
+		exit(-1);
+	}
+	else{
+		XSetForeground(display, black_gc, black_col.pixel);
+	}
+
+	blue_gc = XCreateGC(display, win, 0, 0);
+	XParseColor(display, colormap, blue, &blue_col);	
+	XSetLineAttributes(display, blue_gc, 4, LineSolid, CapRound, JoinRound);
+	if (XAllocColor(display, colormap, &blue_col) == 0){
+		printf("Failed to get color blue\n");
+		exit(-1);
+	}
+	else{
+		XSetForeground(display, blue_gc, blue_col.pixel);	
+	}
+
+	return 0;
+}
